b977239324ef6f315e1ff7958acf098a3dc3ddbd
LUTHFI FADHULLAH RASYID <luthfi.rasyid@binus.ac.id>
2023-10-08 02:16:51 +0700

    login layout done

---
+++ app/src/main/java/com/example/tukang_in/loginActivity.java
@@ -0,0 +1,13 @@
+package com.example.tukang_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

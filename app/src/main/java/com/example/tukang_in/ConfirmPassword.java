9dce754a8517d3a304d213da6ca544437017f9cc
LUTHFI FADHULLAH RASYID <luthfi.rasyid@binus.ac.id>
2023-10-08 10:32:54 +0700

    login,regis,confirm pass layout done

---
+++ app/src/main/java/com/example/tukang_in/ConfirmPassword.java
@@ -0,0 +1,14 @@
+package com.example.tukang_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_password);
+    }
+}

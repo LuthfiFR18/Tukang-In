c9b7c2f5dc0f9cf3ee0b377d0a42c7fe57c231be
LUTHFI FADHULLAH RASYID <luthfi.rasyid@binus.ac.id>
2023-11-17 13:55:53 +0700

    17/11

--- app/src/main/java/com/example/tukang_in/RegisterActivity.java
+++ app/src/main/java/com/example/tukang_in/RegisterActivity.java
@@ -2,13 +2,62 @@ package com.example.tukang_in;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.tukang_in.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private ActivityRegisterBinding binding;
+    private boolean isPasswordMismatch = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListener();
     }
+
+    private void setListener(){
+        binding.txtmasuk.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), loginActivity.class));
+        });
+        binding.btnDaftar1.setOnClickListener(v -> {
+            register();
+        });
+
+    }
+
+    private void register() {
+        String password = binding.regisPass.getText().toString();
+        String confirmPassword = binding.ConfirmPass.getText().toString();
+        TextView passwordMatchText = binding.passwordMatchText;
+
+
+        if (!password.isEmpty() && password.length() > 4) {
+            if (password.equals(confirmPassword)) {
+
+                isPasswordMismatch = false;
+                passwordMatchText.setText("Password sesuai");
+                passwordMatchText.setTextColor(getResources().getColor(R.color.black));
+
+            } else {
+                isPasswordMismatch = true;
+                passwordMatchText.setText("Password tidak sesuai");
+                passwordMatchText.setTextColor(getResources().getColor(R.color.red));
+               // startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        }
+        else {
+            // Password is empty or less than 6 characters
+            passwordMatchText.setText("Password setidaknya 5 huruf");
+            passwordMatchText.setTextColor(getResources().getColor(R.color.red));
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/tukang_in/loginActivity.java
+++ app/src/main/java/com/example/tukang_in/loginActivity.java
@@ -1,13 +1,40 @@
 package com.example.tukang_in;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.location.GnssAntennaInfo;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.tukang_in.databinding.ActivityLoginBinding;
+
 
 public class loginActivity extends AppCompatActivity {
+
+
+  private ActivityLoginBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListener();
+
     }
+
+    private void setListener() {
+        binding.txtregistrasi.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+        });
+        binding.btnmasuk1.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        });
+    }
+
+
 }
